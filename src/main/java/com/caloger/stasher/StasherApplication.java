dfa56e30754be3bc82ddd420b5de3b5e4eecbceb
Matthew Caloger <mattcaloger@gmail.com>
2022-02-20 23:29:34 -0500

    Initial commit

---
+++ src/main/java/com/caloger/stasher/StasherApplication.java
@@ -0,0 +1,13 @@
+package com.caloger.stasher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StasherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caloger/stasher/StasherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caloger.stasher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StasherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
