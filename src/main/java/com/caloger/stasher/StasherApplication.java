d60ab015db6e2f0b2c140bdf0f14fff64704b588
mcaloger <mattcaloger@gmail.com>
2022-03-29 21:38:37 -0400

    Additional UI and config fixes

--- src/main/java/com/caloger/stasher/StasherApplication.java
+++ src/main/java/com/caloger/stasher/StasherApplication.java
@@ -1,11 +1,11 @@
 package com.caloger.stasher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StasherApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StasherApplication.class, args);
 	}
--- web-react-ts/src/Components/Secured/ReadSecured/ReadSecured.tsx
+++ web-react-ts/src/Components/Secured/ReadSecured/ReadSecured.tsx
@@ -4,7 +4,19 @@ import { readSecured } from '../../../API/SecuredAPI';
 import { useParams, Outlet } from 'react-router-dom';
 import { MessageContainer } from '../../MessageContainer/MessageContainer';
 import CreateFooter from '../../CreateFooter/CreateFooter';
-import { Button, PasswordInput, Text, Textarea } from '@mantine/core';
+import { Button, PasswordInput, SimpleGrid, Space, Text, Textarea } from '@mantine/core';
+import styled from '@emotion/styled';
+
+const PasswordBox = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+const SubmitButtonBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+`
 
 export default function ReadSecured() {
 
@@ -27,15 +39,18 @@ export default function ReadSecured() {
         setMessage(message.message)
     }
 
+
     const PasswordEntry = () => {
         return (
-            <div>
-                <label htmlFor="passcode"></label>
-                <div>
+            <PasswordBox>
                     <PasswordInput label="Enter password for secured message" id="passcode" name="passcode" value={password} onChange={handlePasswordChange}></PasswordInput>
-                    <Button onClick={ handleButtonClick }>Submit</Button>
-                </div>
-            </div>
+                    <Space w="sm"></Space>
+                    <SubmitButtonBox>
+                        <Button onClick={ handleButtonClick }>Submit</Button>
+                    </SubmitButtonBox>
+                    
+
+            </PasswordBox>
         )
     }
 
